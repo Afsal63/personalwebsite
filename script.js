87cd4affc2d4368fec5a083da6300d3b7445b545
Afsal KA <afsalafzz63@gmail.com>
2021-12-29 23:05:22 +0530

    first commit

--- script.js
+++ script.js
@@ -1,99 +1,88 @@
-var pname = document.getElementById('name')
-var pmail = document.getElementById('email')
-var Lmesage = document.getElementById('Message')
-var pmobile = document.getElementById('subject')
-var errbox = document.getElementsByClassName('errormsg')
-var submit = document.getElementById('submit-form')
-var charecters = /^[a-zA-Z]+$/
-var mailcode =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-var phonenumber = /^((\+[1-9]{1,4}[ \-])|(\([0-9]{2,3}\)[ \-])|([0-9]{2,4})[ \-])?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
-var def=0
+function validate(){
 
-$("#submit-form").submit((e)=>{
+    if(nameValidate()==true && emailValidate()==true && mobileValidate()==true && messageValidate()==true){
 
-    def=0;
-     e.preventDefault()
-     validation()
-     emailpart()
-     mobilepart()
-     messagepart()
-     if(def==4){
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycbxBWUDYrruN_beevuEJXkJaQRYoTyWe0GDSNUvCiHnEamulrhBhTXcMk--gSdNWgoFN/exec",
+            data:$("#submit-form").serialize(),
+            method:"post",
+            success:function (response){
+                alert("Form submitted successfully")
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error:function (err){
+                alert("Please enter the values")
 
+            }
+        })
 
-     $.ajax({
-         url:"https://script.google.com/macros/s/AKfycbwV3CZni70BnOezJ4c7htuXiuLPUMijejJZ55awpGV5iZdwr1WffJmr3MCnL3o1gNxS9A/exec",
-         data:$("#submit-form").serialize(),
-         method:"post",
-         success:function (response){
-             alert("Form submitted successfully")
-             window.location.reload()
-             //window.location.href="https://google.com"
-         },
-         error:function (err){
-             alert("Something Error")
-
-         }
-     })
-   }
- })
-
-
-
-        // Namepart validation
-
-function validation(){
-    if(pname.value =="" ){
-        errbox[0].innerHTML="Please Enter your Name"
-    }else if(pname.value.trim()==" "){
-        errbox[0].innerHTML="Please do not enter spaces"
-    }else if(charecters.test(pname.value)==false){
-        
-        errbox[0].innerHTML="Please Enter charecters only"
-    }else{
-        errbox[0].innerHTML=""
-        def=def+1;
-    }
-        
-    
-    
-
-}
-
-         // emailpart validation
-
-function emailpart(){
-    if(mailcode.test(pmail.value)==false){
-        errbox[1].innerHTML="Please Enter a valid email id"
-    }else{
-        errbox[1].innerHTML=""
-        def=def+1;
+      }
     }
 
-}
 
-         // mobile number part
+    function nameValidate(){
+      var varname = $('#name').val();
+      var pattern = /^[A-Za-z]/;
 
-
-
-function mobilepart(){
-    var m=pmobile.value
-    if(m.length<10 || m.length>10){
-        errbox[2].innerHTML="Please Enter 10 digit mobile number"
-    }else{
-        errbox[2].innerHTML=""
-        def=def+1;
+      if(varname==""){
+        $('#namecheck').html("Name is mandatory");
+        return false;
+      }else if(varname.match(pattern)){
+        $('#namecheck').html("");
+        return true;
+      }else{
+        $('#namecheck').html("Enter correct name");
+        return false;
+      }
     }
-}
 
+    function emailValidate(){
+      var varEmail = $('#email').val();
+      var emailPattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-        // message part  
-    
-function messagepart(){
-    var k=Lmesage.value
-    if(k.length<10){
-        errbox[3].innerHTML="Please enter atleast 10 words"
-    }else{
-        errbox[3].innerHTML=""
-        def=def+1;
+      if(varEmail==""){
+        $('#emailcheck').html("Email is mandatory");
+        return false;
+      }else if(varEmail.match(emailPattern)){
+        $('#emailcheck').html("");
+        return true;
+      }else{
+        $('#emailcheck').html("Enter correct Email");
+        return false;
+      }
+
     }
-}
+
+    function mobileValidate(){
+      var varMobile = $('#contactid').val();
+      var mobilePattern = /^\d{10}$/;
+
+      if(varMobile==""){
+        $('#numbercheck').html("Contact number is mandatory");
+        return false;
+      }else if(varMobile.match(mobilePattern)){
+        $('#numbercheck').html("");
+        return true;
+      }
+      else if(varMobile.length<10 ||varMobile.length>=10){
+        $('#numbercheck').html("Number should be 10 digits");
+      }
+      else{
+        $('#numbercheck').html("Enter valid Contact number");
+        return false;
+      }
+    }
+
+    function messageValidate(){
+      var varMessage = $('#message').val();
+
+      if(varMessage==""){
+        $('#messagecheck').html("Enter something please");
+        return false;
+      }else {
+        $('#messagecheck').html("");
+        return true;
+      }
+    }
+
